0d0214a2237d588e35adeabd231cc429c6cd4c34
slan <slan.dizier@gmail.com>
2020-01-22 13:27:59 -0800

    Add Piece, Check win and tests

--- src/Game.js
+++ src/Game.js
@@ -7,6 +7,44 @@ export default class Game {
   newGame() {
     this.board = [['', '', ''], ['', '', ''], ['', '', '']];
     this.isWon = false;
+    this.currentPosition = [];
+    this.currentPlayer = 'x';
+  }
+
+  moveTo(row, col) {
+    if (this.board[row][col] === '') {
+      this.addPieceToBoardByLocation(row, col, this.currentPlayer);
+      this.currentPosition = [row, col];
+      this.isWon = this.isWin(this.currentPlayer);
+      this.switchToNextPlayer();
+      return true;
+    }
+    return false;
+  }
+
+  switchToNextPlayer() {
+    if (this.currentPlayer === 'x') {
+      this.currentPlayer = 'y';
+    } else {
+      this.currentPlayer = 'x';
+    }
+  }
+
+  // Gets the last row played
+  getRow() {
+    return this.currentPosition[0];
+  }
+
+  // Gets the last col played
+  getCol() {
+    return this.currentPosition[0];
+  }
+
+  isWin(player) {
+    const rowWin = this.isWinByRowAndPlayer(this.getRow(), player);
+    const colWin = this.isWinByColAndPlayer(this.getCol(), player);
+    const diagWin = this.isDiagnolWinByPlayer(player);
+    return rowWin || colWin || diagWin;
   }
 
   addPieceToBoardByLocation(row, col, playerPiece) {
@@ -25,11 +63,34 @@ export default class Game {
   }
 
   isWinByColAndPlayer(col, player) {
-    const columns = [];
-    columns.push(this.board[0][col]);
-    columns.push(this.board[1][col]);
-    columns.push(this.board[2][col]);
-    return columns.every((val) => val === player);
+    const positions = [];
+    positions.push(this.board[0][col]);
+    positions.push(this.board[1][col]);
+    positions.push(this.board[2][col]);
+
+    return positions.every((val) => val === player);
   }
 
+  isWinByDiagnolLeft(player) {
+    const positions = [];
+    positions.push(this.board[0][0]);
+    positions.push(this.board[1][1]);
+    positions.push(this.board[2][2]);
+
+    return positions.every((val) => val === player);
+  }
+
+  isWinByDiagnolRight(player) {
+    const positions = [];
+    positions.push(this.board[0][2]);
+    positions.push(this.board[1][1]);
+    positions.push(this.board[2][0]);
+
+    return positions.every((val) => val === player);
+  }
+
+  isDiagnolWinByPlayer(player) {
+    return this.isWinByDiagnolLeft(player)
+      || this.isWinByDiagnolRight(player);
+  }
 }
--- src/Game.test.js
+++ src/Game.test.js
@@ -43,10 +43,96 @@ describe('Game rules for tic tac toe', () => {
   describe('Wins by columns', () => {
     it('declares a win for x on the first column', () => {
       game.addPieceToBoardByLocation(0, 0, 'x');
-      game.addPieceToBoardByLocation(0, 1, 'x');
-      game.addPieceToBoardByLocation(0, 2, 'x');
+      game.addPieceToBoardByLocation(1, 0, 'x');
+      game.addPieceToBoardByLocation(2, 0, 'x');
 
-      expect(game.isWinByRowAndPlayer(0, 'x')).toBeTruthy();
+      expect(game.isWinByColAndPlayer(0, 'x')).toBeTruthy();
+    });
+
+    it('declares a win for o on the second column', () => {
+      game.addPieceToBoardByLocation(0, 1, 'o');
+      game.addPieceToBoardByLocation(1, 1, 'o');
+      game.addPieceToBoardByLocation(2, 1, 'o');
+      expect(game.isWinByColAndPlayer(1, 'o')).toBeTruthy();
+    });
+
+    it('declares a win for o on the third column', () => {
+      game.addPieceToBoardByLocation(0, 2, 'o');
+      game.addPieceToBoardByLocation(1, 2, 'o');
+      game.addPieceToBoardByLocation(2, 2, 'o');
+      expect(game.isWinByColAndPlayer(2, 'o')).toBeTruthy();
+    });
+  });
+
+  describe('Wins by diaganols', () => {
+    it('declares a win for x diagnolly starting from top left', () => {
+      game.addPieceToBoardByLocation(0, 0, 'x');
+      game.addPieceToBoardByLocation(1, 1, 'x');
+      game.addPieceToBoardByLocation(2, 2, 'x');
+
+      expect(game.isWinByDiagnolLeft('x')).toBeTruthy();
+    });
+
+    it('declares a win for o diagnolly starting from top right', () => {
+      game.addPieceToBoardByLocation(0, 2, 'o');
+      game.addPieceToBoardByLocation(1, 1, 'o');
+      game.addPieceToBoardByLocation(2, 0, 'o');
+
+      expect(game.isWinByDiagnolRight('o')).toBeTruthy();
+    });
+
+    it('declares a win for o diagnolly', () => {
+      game.addPieceToBoardByLocation(0, 2, 'o');
+      game.addPieceToBoardByLocation(1, 1, 'o');
+      game.addPieceToBoardByLocation(2, 0, 'o');
+
+      expect(game.isDiagnolWinByPlayer('o')).toBeTruthy();
+    });
+
+    it('declares a win for x diagnolly', () => {
+      game.addPieceToBoardByLocation(0, 0, 'x');
+      game.addPieceToBoardByLocation(1, 1, 'x');
+      game.addPieceToBoardByLocation(2, 2, 'x');
+
+      expect(game.isDiagnolWinByPlayer('x')).toBeTruthy();
+    });
+  });
+
+  describe('Moves can only be valid', () => {
+    it('does not allow y to move to the same location as x', () => {
+      expect(game.moveTo(0, 0)).toBeTruthy();
+      expect(game.moveTo(0, 0)).toBeFalsy();
+      // Make sure that it's the same player since the move was invalid
+      expect(game.currentPlayer).toBe('y');
+    });
+  });
+
+  describe('Playing a game', () => {
+    it('Plays x to a win', () => {
+      game.moveTo(1, 1);
+      expect(game.isWin('x')).toBeFalsy();
+      game.moveTo(0, 0);
+      expect(game.isWin('y')).toBeFalsy();
+      game.moveTo(0, 2);
+      expect(game.isWin('x')).toBeFalsy();
+      game.moveTo(0, 1);
+      expect(game.isWin('y')).toBeFalsy();
+      game.moveTo(2, 0);
+      expect(game.isWin('x')).toBeTruthy();
+    });
+
+    it('Plays y to a win', () => {
+      game.moveTo(1, 1);
+      expect(game.isWin('x')).toBeFalsy();
+      game.moveTo(0, 0);
+      expect(game.isWin('y')).toBeFalsy();
+      game.moveTo(2, 2);
+      expect(game.isWin('x')).toBeFalsy();
+      game.moveTo(0, 1);
+      expect(game.isWin('y')).toBeFalsy();
+      game.moveTo(2, 0);
+      game.moveTo(0, 2);
+      expect(game.isWin('y')).toBeTruthy();
     });
   });
 });
